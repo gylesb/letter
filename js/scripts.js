f01efa08b496cc18951f970ced856eb6a2188d88
Gyles Batara and Marcus Parmentier <gyles@>
2017-09-13 09:02:06 -0700

    Make everything functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
+
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
-    var nameInput = $("input#name").val();
+
+  $("#formOne").submit(function(event) {
+    var name1Input = $("input#name1").val();
     var name2Input = $("input#name2").val();
 
-    $(".name").text(nameInput);
+    $(".name1").text(name1Input);
     $(".name2").text(name2Input);
 
     $("#story").show();
 
     event.preventDefault();
   });
+
 });
